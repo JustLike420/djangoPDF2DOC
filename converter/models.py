130da9304592d5d69ec03625c417194f4682c761
Vladimir <justlikevova@gmail.com>
2022-09-05 01:02:53 +0300

    str in UsersFiles model

--- converter/models.py
+++ converter/models.py
@@ -5,6 +5,9 @@ from django.conf import settings
 # Create your models here.
 
 class UsersFiles(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
     file = models.FileField()
-    ordered_date = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.created_date) + ' | ' + self.file.name
